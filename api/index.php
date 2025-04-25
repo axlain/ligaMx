de4fa7da54ce7352dafa03106c1d4b345277d5b1
Alain Vasquez Ramirez <zs22016095@estudiantes.uv.mx>
2025-04-25 17:48:14 -0600

    v2

--- api/index.php
+++ api/index.php
@@ -5,7 +5,11 @@ $request_method = $_SERVER['REQUEST_METHOD'];
 
 if (strpos($request_uri, '/api/equipo') === 0) {
     include '../src/equipo/index.php';
-} else {
+} else if(strpos($request_uri, '/api/jugador') === 0){
+    include '../src/jugador/index.php';
+}
+
+else {
     header("HTTP/1.1 404 Not Found");
 }
 ?>
